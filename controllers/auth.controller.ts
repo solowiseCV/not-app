cd8de03dc244495deeb641fdf74ee2e495a2c6f8
solowiseCv <uchesolomon61@gmail.com>
2025-03-24 00:27:16 +0100

    updated

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
 import { LoginCredentials, RegisterCredentials } from '../interfaces/auth.interface';
 import NoteServices from '../services/service';
